ac1ba7b0e026ca7d24272eb5497ed8312249fc96
joura <martinezluisjorge86@gmail.com>
2019-04-09 19:38:23 -0400

    Updates

---
+++ precache-manifest.622a743f169c5861c3eeb5205aec095e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f4c3e7b4a6cde56f59d",
+    "url": "/xMarksTheSpot/static/js/runtime~main.3edd0718.js"
+  },
+  {
+    "revision": "3255e7f1ac36e230719e",
+    "url": "/xMarksTheSpot/static/js/main.30d84a9c.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/xMarksTheSpot/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3255e7f1ac36e230719e",
+    "url": "/xMarksTheSpot/static/css/main.c53462a6.chunk.css"
+  },
+  {
+    "revision": "3e8a21cd34a4affb8cd1bda9b752d821",
+    "url": "/xMarksTheSpot/index.html"
+  }
+];
